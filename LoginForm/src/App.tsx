efd77f1a5b017146e4f4256931b9eb0df689e634
Gulshan043 <gulshanjain043@gmail.com>
2026-05-28 08:09:55 +0530

    added form handling

--- LoginForm/src/App.tsx
+++ LoginForm/src/App.tsx
@@ -1,23 +1,25 @@
-import { useState } from "react"
+// import { useState } from "react"
 import LoginPage from "./pages/Login/loginPage"
 
 const App = () => {
-  const [obj, setobj] = useState({name:'Gulshan', Age: 23});
+  // const [obj, setobj] = useState({name:'Gulshan', Age: 23});
 
-  const onClickBtn = () =>{
-    const newObj = {...obj};
-    console.log("newObj: ", newObj);
-    newObj.name = "Gulshan Sethi";
-    setobj(newObj);
+  // const onClickBtn = () =>{
+  //   const newObj = {...obj};
+  //   console.log("newObj: ", newObj);
+  //   newObj.name = "Gulshan Sethi";
+  //   setobj(newObj);
     
-  }
-  return (
-    <>
-    {/* <LoginPage /> */}
-    <h1>Name:{obj.name} and Age{obj.Age}</h1>
-    <button className="bg-blue-300 p-5 m-5 rounded-full" onClick={onClickBtn}>Change Values</button>
-    </>
-  )
+  // }
+  // return (
+  //   <>
+  //   {/* <LoginPage /> */}
+  //   <h1>Name:{obj.name} and Age{obj.Age}</h1>
+  //   <button className="bg-blue-300 p-5 m-5 rounded-full" onClick={onClickBtn}>Change Values</button>
+  //   </>
+  // )
+
+  return <LoginPage/>
 }
 
 export default App
--- LoginForm/src/pages/Login/components/formContainer.tsx
+++ LoginForm/src/pages/Login/components/formContainer.tsx
@@ -1,15 +1,45 @@
-import Btn from "./btn";
-import FormInputs from "./formInputs";
-
+// import Btn from "./btn";
+// import FormInputs from "./formInputs";
+import { useState } from "react";
 const FormContainer = () => {
+
+  const [input, setInput] = useState('second');
+
+  const submitHandler = (e:any) => {
+    e.preventDefault();
+    console.log("form submitted by: ", input);
+    setInput('')
+  };
+
+  const onInput = (e:any) => {
+    setInput(e.target.value);
+  };
   return (
-    <div className="border-2 border-white rounded-xl pt-0 p-6 w-md max-w-md">
-      <h1 className="text-white my-4 text-center font-bold">Login</h1>
-      <form>
-        <FormInputs />
-        <Btn />
-      </form>
-    </div>
+    // <div className="border-2 border-white rounded-xl pt-0 p-6 w-md max-w-md">
+    //   <h1 className="text-white my-4 text-center font-bold">Login</h1>
+    //   <form>
+    //     <FormInputs />
+    //     <Btn />
+    //   </form>
+    // </div>
+
+    <form
+      className="flex flex-col gap-4"
+      onSubmit={(e) => {
+        submitHandler(e);
+      }}
+    >
+      <input
+        type="text"
+        onChange={(e)=> onInput(e)}
+        value={input}
+        className="border border-white rounded-2xl  text-white p-3"
+      />
+      <button
+        className="bg-gray-600 px-5 py-2 text-white rounded" type="submit">
+        submit
+      </button>
+    </form>
   );
 };
 
